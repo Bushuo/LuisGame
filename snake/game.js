f6881fb22b4052db44bd666475bc832b1a25e533
KIM <kimBORAM@eduroam-241-095.wlan.univie.ac.at>
2018-06-15 15:28:14 +0200

    add obstacle spawning with falling from sky impression

--- snake/game.js
+++ snake/game.js
@@ -1,6 +1,7 @@
 var scl = 20;
 var snake;
 var food;
+var obs;
 
 function setup() {
 	createCanvas(600,600);
@@ -8,14 +9,37 @@ function setup() {
 	snake = new Snake();
 	food = new Food();
 	food.pickLocation();
+	obs = new Obstacle();
+	obs.reset();
 };
 
 function draw() {
-	background(51);
+	background(125);
+
+	// update gamelogic
 	if(snake.eat(food.pos)){
 		food.pickLocation();
 	}
 	snake.update();
+	if(snake.total >= 10 && !obs.isSpawned && !obs.isFalling) {
+		obs.reset();
+		obs.fall();
+	}
+	obs.update();
+
+	// show gameobjects
 	snake.show();
 	food.show();
+	obs.show();
 };
+
+// debugging area
+
+var spawnObsManually = function () {
+	obs.reset();
+	obs.fall();
+};
+
+function mousePressed() {
+	spawnObsManually();
+}
--- snake/snake.js
+++ snake/snake.js
@@ -9,28 +9,38 @@ Snake = function() {
     this.total = 0;
     this.tail = [];
 
-    this.death = function() {
+	// check if the snake bit itself in the tail
+    this.selfKill = function() {
         for(var i = 0; i < this.tail.length; i++ ) {
             var pos = this.tail[i];
             var d = dist(this.x, this.y, pos.x, pos.y);
             if(d < 1) {
-                this.total = 0;
-                this.tail = [];
+				this.reset();
+				obs.reset();
             }
         }
-    }
+    };
 
+	// reset the sanke
+	this.reset = function () {
+		this.total = 0;
+		this.tail = [];
+	};
+
+	// move the snake
+	// update the tail array
+	// check for out of gamefield
 	this.update = function() {
 		if(!running) {
 			return;
 		}
-		this.death();
-    if(this.total === this.tail.length) {
-        for(var i = 0; i < this.tail.length-1; i++) {
-            this.tail[i] = this.tail[i+1];
-        }
-    }
-    this.tail[this.total-1] = createVector(this.x, this.y);
+		this.selfKill();
+	    if(this.total === this.tail.length) {
+	        for(var i = 0; i < this.tail.length-1; i++) {
+	            this.tail[i] = this.tail[i+1];
+	        }
+	    }
+	    this.tail[this.total-1] = createVector(this.x, this.y);
 
 		this.x += this.xSpeed * scl;
         this.y += this.ySpeed * scl;
@@ -39,6 +49,7 @@ Snake = function() {
         this.y = constrain(this.y, 0, height-scl);
     };
 
+	// check for overlapping with food
     this.eat = function(pos) {
         var d = dist(this.x, this.y, pos.x, pos.y);
         if(d < 1) {
@@ -49,6 +60,7 @@ Snake = function() {
         }
     };
 
+	// display the snake
 	this.show = function() {
 		fill(230,230,230);
         rect(this.x, this.y, scl, scl);
@@ -61,8 +73,9 @@ Snake = function() {
 		text(this.total, scl, scl+10);
 	};
 
+	// direction of the snake
 	this.dir = function(dirX, dirY) {
 		this.xSpeed = dirX;
 		this.ySpeed = dirY;
-	}
+	};
 }
